425a9359c5e81cba3042bc9d1f8ccc4f5b8e6329
Gabriel Trindade <52544804+gvtrindade@users.noreply.github.com>
2019-11-01 14:41:59 -0300

    First commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+	resposta();
+}
+
+function resposta(){
+	const resposta = document.getElementById("resposta");
+	Array.prototype.rand = function(){
+	return this[Math.floor(Math.random() * this.length)];
+	};
+	resposta.innerText = ["Yes","No"].rand();
+}
